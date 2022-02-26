2c5cedf8e28486dcc2720dc5328f0ac55098b154
Ayesha Khan <nawal.khan@northsouth.edu>
2022-02-27 00:09:21 +0600

    Models updated

--- MusicApp/RecApp/models.py
+++ MusicApp/RecApp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class User(models.Model):
-
     name = models.CharField(max_length=150)
     age = models.IntegerField()
     gender = models.CharField(max_length=20)
