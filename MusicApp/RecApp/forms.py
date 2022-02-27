d10068c0e6a9edefa7d0f5b9e122abd347b05905
Ayesha Khan <nawal.khan@northsouth.edu>
2022-02-28 03:01:56 +0600

    Forms updated

--- MusicApp/RecApp/forms.py
+++ MusicApp/RecApp/forms.py
@@ -6,8 +6,11 @@ from .models import CustomUser
 # Create your forms here.
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model=CustomUser
-        fields = "__all__"
-
+    #class Meta:
+        #model=CustomUser
+        #fields = "__all__"
+    class Meta(UserCreationForm.Meta):
+        #keeps additional information about the form and extends UserCreationForm
+        fields = UserCreationForm.Meta.fields + ("email",)        
+      
       
