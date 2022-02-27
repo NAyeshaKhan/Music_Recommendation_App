a3f2c3e4e11a769038439d6b3e059733cccff9f6
Ayesha Khan <nawal.khan@northsouth.edu>
2022-02-28 00:32:04 +0600

    Forms modified

--- MusicApp/RecApp/forms.py
+++ MusicApp/RecApp/forms.py
@@ -6,9 +6,9 @@ from django.contrib.auth.models import User
 # Create your forms here.
 
 class NewUserForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, required=True, help_text='Email')
-    age = forms.IntegerField(required=True,help_text='Age')
-    gender = forms.CharField(max_length=20, required=True, help_text='Gender')
+    email = forms.EmailField(max_length=200, required=True)
+    age = forms.IntegerField(required=True)
+    gender = forms.CharField(max_length=20, required=True)
 
     class Meta:
         model = User
