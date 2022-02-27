1eac3baf9a1f69b61c456ebc0b035f307e3e006d
Ayesha Khan <nawal.khan@northsouth.edu>
2022-02-28 03:02:21 +0600

    View causes error due to new model

--- MusicApp/RecApp/views.py
+++ MusicApp/RecApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import NewUserForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
@@ -9,18 +9,17 @@ from django.contrib import messages
 def signup(request):
     if request.method == "GET":
         return render(
-            request, "signup.html", {"form": NewUserForm}
+            request, "signup.html", {"form": CustomUserCreationForm}
         )
     elif request.method == "POST":
-        form = NewUserForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             #email = form.cleaned_data.get('email')
             #password = form.cleaned_data.get('password1')
-            #user = authenticate(email=email, password=password)
             login(request, user)
             return redirect("RecApp:dashboard")
-     
+          
      
 def home(request):
     return render(request, 'home.html')
@@ -32,7 +31,6 @@ def signin(request):
         if form.is_valid():
             email = form.cleaned_data.get('email')
             password = form.cleaned_data.get('password')
-            user = authenticate(email=email, password=password)
             if user is not None:
                 login(request, user)
                 return redirect("RecApp:dashboard")
