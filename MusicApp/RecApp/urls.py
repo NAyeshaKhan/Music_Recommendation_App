bafaea4b01033dc670dbb8e566880fdc9cdddee1
Ayesha Khan <nawal.khan@northsouth.edu>
2022-02-28 00:32:16 +0600

    Urls modified

--- MusicApp/RecApp/urls.py
+++ MusicApp/RecApp/urls.py
@@ -3,7 +3,7 @@ from RecApp import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('signup', views.signup, name='signup'),
-    path('signin', views.signin, name='signin'),
-    path('dashboard', views.dashboard, name='dashboard')
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('dashboard/', views.dashboard, name='dashboard')
 ]
