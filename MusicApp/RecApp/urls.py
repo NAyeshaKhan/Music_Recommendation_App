200f2b31ffd588579201c2e5263bea33501f3d74
Ayesha Khan <nawal.khan@northsouth.edu>
2022-02-28 14:06:03 +0600

    Logout url and view added

--- MusicApp/RecApp/urls.py
+++ MusicApp/RecApp/urls.py
@@ -5,8 +5,8 @@ app_name = "RecApp"
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path("register", views.register_request, name="register"),
-    path("login", views.login_request, name="login"),
-    #path("logout", views.logout_request, name= "logout"),
+    path("register/", views.register_request, name="register"),
+    path("login/", views.login_request, name="login"),
+    path("logout/", views.logout_request, name= "logout"),
     path('dashboard/', views.dashboard, name='dashboard')
 ]
