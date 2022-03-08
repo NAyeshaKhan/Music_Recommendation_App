0544c01bfd1985fe2718e637a186d1ed19045f26
Sarah-Jasim <89269184+Sarah-Jasim@users.noreply.github.com>
2022-03-08 23:46:44 +0600

    Update urls.py

--- MusicApp/RecApp/urls.py
+++ MusicApp/RecApp/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 from rest_framework import routers
+from .views import SearchResultsView
 
 app_name = "RecApp"
 router = routers.DefaultRouter()
