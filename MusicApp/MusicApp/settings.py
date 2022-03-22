66f5e8390dc2fb54ad116cda484ccaa8db9f95c4
Seam E Nur <57158514+seamenur@users.noreply.github.com>
2022-03-23 00:43:04 +0600

    Database changed back to mysql

--- MusicApp/MusicApp/settings.py
+++ MusicApp/MusicApp/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'MusicApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'music_app',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
