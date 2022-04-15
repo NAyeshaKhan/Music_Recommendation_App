598eba6546890c27f024136178bba655cff43cc0
Seam E Nur <57158514+seamenur@users.noreply.github.com>
2022-04-15 17:18:47 +0600

    Update settings.py

--- MusicApp/MusicApp/settings.py
+++ MusicApp/MusicApp/settings.py
@@ -126,10 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
 
