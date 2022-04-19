6ac767668723b4360c07b5573daa22aad3a4c2f9
Sarah-Jasim <89269184+Sarah-Jasim@users.noreply.github.com>
2022-04-20 04:17:19 +0600

    updated urls.py

--- MusicApp/RecApp/urls.py
+++ MusicApp/RecApp/urls.py
@@ -40,7 +40,11 @@ urlpatterns = [
     path('reset_password_complete/',
          auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"),
          name="password_reset_complete"),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+         name='password_change'),
 
+    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+         name='password_change_done'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
